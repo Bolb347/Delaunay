ba4d7bae76f045b8e95bf7d5dde74737d0774e8f
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2024-05-24 10:55:24 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
+#include "delaunay.h"
+#include "types.h"
+#include <vector>
 
+int main() {
+    std::vector<Vec2> cloud = {(Vec2){1, 1}, (Vec2){1, -1}, (Vec2){-1, 1}, (Vec2){-1, -1}};
+    std::vector<Triangle2> triangulated = get_bowyer_watson(cloud);
+    return 0;
+};
