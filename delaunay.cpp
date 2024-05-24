a54699aebf552e7be325aa41f7b518a97bd04e94
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2024-05-24 10:30:53 -0700

    Update delaunay.cpp

--- delaunay.cpp
+++ delaunay.cpp
@@ -4,10 +4,33 @@
 #include <vector>
 #include <limits>
 
-#define POW(x, y) std::pow(x, y)
+#define SQR(x) std::pow(x, 2)
 #define ABS(x) std::abs(x)
 #define SQRT(x) std::sqrt(x)
 #define INF std::numeric_limits<float>::max()
+
+float distance(const Vec2 &a, const Vec2 &b) {
+    return SQRT(SQR(b.x - a.x) + SQR(b.y - a.y));
+}
+
+Vec2 get_circumcenter(const Triangle2 &triangle) {
+    Vec2 A = triangle.m_p1;
+    Vec2 B = triangle.m_p2;
+    Vec2 C = triangle.m_p3;
+    float D = 2 * ((A.x * (B.y - C.y)) + (B.x * (C.y - A.y)) + (C.x * (A.y - B.y)));
+    float x = (1/D) * (((SQR(A.x) + SQR(A.y)) * (B.y - C.y)) + ((SQR(B.x) + SQR(B.y)) * (C.y - A.y)) + ((SQR(C.x) + SQR(C.y)) * (A.y - B.y)));
+    float y = (1/D) * (((SQR(A.x) + SQR(A.y)) * (C.x - B.x)) + ((SQR(B.x) + SQR(B.y)) * (A.x - C.x)) + ((SQR(C.x) + SQR(C.y)) * (B.x - A.x)));
+    return {x, y};
+}
+
+bool in_circumcircle(const Triangle2 &triangle, const Vec2 &point) {
+    Vec2 center = get_circumcenter(triangle);
+    if (distance(center, point) > distance(center, triangle.m_p1)) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
 std::array<Vec2, 3> get_points(const Triangle2 &triangle) {
     return {triangle.m_p1, triangle.m_p2, triangle.m_p3};
@@ -15,6 +38,18 @@ std::array<Vec2, 3> get_points(const Triangle2 &triangle) {
 
 std::array<Edge, 3> get_edges(const Triangle2 &triangle) {
     return {(Edge){triangle.m_p1, triangle.m_p2}, (Edge){triangle.m_p2, triangle.m_p3}, (Edge){triangle.m_p3, triangle.m_p1}};
+}
+
+std::vector<Vec2> get_shared_points(const Triangle2 &a, const Triangle2 &b) {
+    std::vector<Vec2> shared;
+    for (Vec2 &point_a : get_points(a)) {
+        for (Vec2 &point_b : get_points(b)) {
+            if (point_a == point_b) {
+                shared.emplace_back(point_a);
+            }
+        }
+    }
+    return shared;
 }
 
 bool operator==(const Vec2 &a, const Vec2 &b) {
@@ -53,13 +88,14 @@ void remove_from_vec(std::vector<Triangle2> *vector, const Triangle2 &val) {
     vector->erase(std::remove(vector->begin(), vector->end(), val), vector->end());
 }
 
-std::vector<Triangle2> get_bowyer_watson(std::vector<Vec2> pointList) {
+std::vector<Triangle2> get_bowyer_watson(const std::vector<Vec2> &pointList) {
     std::vector<Triangle2> triangulation;
-    triangulation.emplace_back((Triangle2){(Vec2){-INF, -INF}, (Vec2){INF, -INF}, (Vec2){0, INF}}); // must be large enough to completely contain all the points in pointList
+    Triangle2 super = {(Vec2){-INF, -INF}, (Vec2){INF, -INF}, (Vec2){0, INF}};
+    triangulation.emplace_back(super); // must be large enough to completely contain all the points in pointList
     for (Vec2 &point : pointList) { // add all the points one at a time to the triangulation
         std::vector<Triangle2> badTriangles;
         for (Triangle2 &triangle : triangulation) { // first find all the triangles that are no longer valid due to the insertion
-            if () {///point is inside circumcircle of triangle
+            if (in_circumcircle(triangle, point)) {
                 badTriangles.emplace_back(triangle);
         }
         std::vector<Edge> polygon;
@@ -76,7 +112,7 @@ std::vector<Triangle2> get_bowyer_watson(std::vector<Vec2> pointList) {
             triangulation.emplace_back((Triangle2){edge.m_p1, edge.m_p2, point}) // form a triangle from edge to point and add it to triangulation
     }
     for (Triangle2 &triangle : triangulation) { // done inserting points, now clean up
-        if () {///triangle contains a vertex from original super-triangle
+        if (get_shared_points(triangle, super).size() > 0) {
             remove_from_vec(&triangulation, triangle);
         }
     }
