87b17be7ab9c1778ea44b19482d5a9350123394f
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2024-05-25 08:38:20 -0700

    Update delaunay.h

--- delaunay.h
+++ delaunay.h
@@ -5,25 +5,25 @@
 #include <array>
 #include <vector>
 
-float distance(const Vec2 &a, const Vec2 &b);
+float distance(Vec2 a, Vec2 b);
 
-Vec2 get_circumcenter(const Triangle2 &triangle);
+Vec2 get_circumcenter(Triangle2 triangle);
 
-bool in_circumcircle(const Triangle2 &triangle, const Vec2 &point);
+bool in_circumcircle(Triangle2 triangle, Vec2 point);
 
-std::array<Vec2, 3> get_points(const Triangle2 &triangle);
+std::array<Vec2, 3> get_points(Triangle2 triangle);
 
-std::array<Edge, 3> get_edges(const Triangle2 &triangle);
+std::array<Edge, 3> get_edges(Triangle2 triangle);
 
-std::vector<Vec2> get_shared_points(const Triangle2 &a, const Triangle2 &b);
+std::vector<Vec2> get_shared_points(Triangle2 a, Triangle2 b);
 
 bool operator==(const Vec2 &a, const Vec2 &b);
 bool operator==(const Triangle2 &a, const Triangle2 &b);
 bool operator==(const Edge &a, const Edge &b);
 
-void remove_from_vec(std::vector<Vec2> *vector, const Vec2 &val);
-void remove_from_vec(std::vector<Edge> *vector, const Edge &val);
-void remove_from_vec(std::vector<Triangle2> *vector, const Triangle2 &val);
+void remove_from_vec(std::vector<Vec2> *vector, Vec2 val);
+void remove_from_vec(std::vector<Edge> *vector, Edge val);
+void remove_from_vec(std::vector<Triangle2> *vector, Triangle2 val);
 
 std::vector<Edge> get_unshared_edges(std::vector<Triangle2> triangles);
 
