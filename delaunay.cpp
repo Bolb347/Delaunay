ddfedc91276fe35b83bcca09b01c6acd0dd7733d
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2024-05-25 19:08:07 -0700

    Update delaunay.cpp

--- delaunay.cpp
+++ delaunay.cpp
@@ -116,7 +116,7 @@ std::vector<Triangle2> get_bowyer_watson(std::vector<Vec2> point_list) {
         std::vector<Edge> polygon;
         for (Triangle2 &triangle : bad_triangles) {
             for (Edge &edge : get_edges(triangle)) {
-                if (!(get_if_unshared(bad_triangles, triangle, edge))) {
+                if (get_if_unshared(bad_triangles, triangle, edge)) {
                     polygon.emplace_back(edge);
                 }
             }
